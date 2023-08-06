39553a8fe97929bb8e06df68121a1fc851ad602f
csullins <camilesullins@gmail.com>
2023-08-06 13:31:50 -0500

    fixing folder structure

--- giant-robot-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders registration form', () => {
-  render(<App />);
-  const form = screen.getByText("First Name");
-  expect(form).toBeInTheDocument();
-});
